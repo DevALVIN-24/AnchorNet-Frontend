fe688edb425f04deb2b81545ffc872d87405c907
jagadeeshftw <jagadeesh26062002@gmail.com>
2026-06-12 17:47:20 +0530

    feat(ui): render SiteFooter on every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WalletProvider } from "@/components/WalletProvider";
+import { SiteFooter } from "@/components/SiteFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <WalletProvider>{children}</WalletProvider>
+        <WalletProvider>
+          {children}
+          <SiteFooter />
+        </WalletProvider>
       </body>
     </html>
   );
